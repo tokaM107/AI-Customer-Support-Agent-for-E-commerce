0388130d44b08ca347d53710badc165aa924dffa
tokam107 <CDS.ToqaMuhammad23795@alexu.edu.eg>
2026-07-19 16:15:45 +0300

    init files

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+"""Application package for the Noon customer support RAG project."""
+
+from .api import app
+
+__all__ = ["app"]
---
+++ app/rag/__init__.py
@@ -0,0 +1,3 @@
+"""LangChain-based RAG utilities for the Noon customer support project."""
+
+from .chain import answer_question, build_answer_runnable, run_cli
